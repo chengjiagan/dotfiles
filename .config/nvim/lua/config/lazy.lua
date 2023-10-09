39e6b05e8eea7785462848e2ea000e648c42fa0a
Jiagan Cheng <waterlemongan@outlook.com>
2023-10-09 14:19:46 +0800

    [nvim] update

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -6,7 +6,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
-local colorscheme = "one_monokai"
+local colorscheme = "tokyonight"
 
 require("lazy").setup({
   spec = {

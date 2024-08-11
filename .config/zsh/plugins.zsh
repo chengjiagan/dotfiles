b7b430ddc107ba53f0423ba71829464ba6a38f0a
Jiagan Cheng <waterlemongan@outlook.com>
2024-08-11 17:25:21 +0800

    migrate back to p10k

--- .config/zsh/plugins.zsh
+++ .config/zsh/plugins.zsh
@@ -12,7 +12,15 @@ source $ZNAP_ROOT/znap.zsh  # Start Znap
 [[ "${TERM_PROGRAM}" = "iTerm.app" ]] && \
     znap eval iterm2 'curl -fsSL https://iterm2.com/shell_integration/zsh'
 
-# Starship prompt
+
+# powerlevel10k prompt
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+znap source romkatv/powerlevel10k
 
 # oh-my-zsh libraries and plugins
 znap source ohmyzsh/ohmyzsh lib/{clipboard,completion} plugins/gitignore
@@ -24,7 +32,6 @@ znap source zimfw/input
 znap source zimfw/utility
 
 # eval
-(( $+commands[starship] )) && znap eval starship 'starship init zsh'
 (( $+commands[docker] )) && znap eval docker 'docker completion zsh'
 (( $+commands[pyenv] )) && znap eval pyenv 'pyenv init -'
 (( $+commands[pyenv-virtualenv] )) && znap eval pyenv-virtualenv 'pyenv virtualenv-init -'
@@ -36,3 +43,5 @@ znap source zsh-users/zsh-autosuggestions
 znap source zsh-users/zsh-history-substring-search
 znap source Aloxaf/fzf-tab
 znap source zdharma-continuum/fast-syntax-highlighting
+
+source ~/.p10k.zsh
